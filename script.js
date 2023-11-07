0566fc7809b91a20a2ccdaaeb92bb8d49e7e0c7e
Lucia MQ <103491565+luquiceno@users.noreply.github.com>
2023-11-07 17:12:27 +0100

    Update script.js
    clean the code

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(response => response.json())
     .then(data => {
-        //console.log(data);
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
 })
@@ -13,8 +12,6 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(response =>  {
-        // when the response is not ok, the code status is 
-        // 400 o 500 something, throw an error
         if (!response.ok) {
             throw Error("Something went wrong");
         }
@@ -46,8 +43,6 @@ setInterval(getCurrentTime, 1000);
 navigator.geolocation.getCurrentPosition(position => {
     fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
         .then(response => {
-            // when the response is not ok, the code status is 
-            // 400 o 500 something, throw an error
             if(!response.ok) {
                 throw Error("Weather data not available");
             }
@@ -65,40 +60,3 @@ navigator.geolocation.getCurrentPosition(position => {
         .catch(err => console.log(err));
 });
 
-
-/*
-MANEJO DE ERRORES DE UNA PROMISE O CUANDO UNA PROMISE TIENE ESTADO REJECTED
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=natureafdasfdsadf")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        throw Error("es un error");
-        //document.body.style.backgroundImage = `url(${data.urls.full})`
-        //document.getElementById("author").textContent = `By: ${data.user.name}`
-})
-.catch (err => {
-    console.log("algo anda mal")
-    document.body.style.backgroundImage = "url('img/pass.jpg')";
-})
-*/
-
-/*
-*** Geolocation position code using callback
-
-navigator.geolocation.getCurrentPosition(position => {
-    console.log(position)
-});
-
-*** Geolocation position code using a Promise
-navigator.geolocation.getCurrentPosition()
-    .then(position => {
-        console.log(position)
-    })
-
-***  Geolocation position code using the async way syntax, also as a Promise
-    
-async function getLocation() {
-    const position = await navigator.geolocation.getCurrentPosition()
-    console.log(position)
-}
-*/
